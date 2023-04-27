ec91eceb88b8a9dc02e0b75fa38392e8ff0ec0ee
Yash Baddi <yashbaddi29@gmail.com>
2023-04-27 10:51:55 +0530

    Fixed Response

--- response.js
+++ response.js
@@ -5,26 +5,34 @@ export function response(socket) {
     ok: true,
   };
   const headers = {
-    date: new Date().toISOString(),
-    "last-modified": new Date().toISOString(),
-    "Accept-Ranges": "bytes",
-    "Content-Length": 1024,
-    Connection: "Keep-Alive",
-    "Content-Type": "text/plain",
+    "Content-Type": "plain/text",
+    "Content-Length": 26,
+    Connection: "keep-alive",
+    "Keep-Alive": "timeout=5",
+    Date: new Date(),
   };
+  let length = 0;
 
-  let sentString = `HTTP/${res.version} ${res.status} ${res.ok ? "OK" : ""}\r\n
-    ${Object.entries(headers)
-      .map(([key, value]) => `${key}:${value}`)
-      .join("\r\n")}\r\n\r\n`;
+  let sentString = `HTTP/${res.version} ${res.status} ${
+    res.ok ? "OK" : ""
+  }\r\n${Object.entries(headers)
+    .map(([key, value]) => `${key}:${value}`)
+    .join("\r\n")}\r\n\r\n`;
 
   function write(content) {
+    // sentString = sentString + length.toString() + "\r\n";
     sentString += content;
   }
   function end() {
+    console.log(sentString);
     socket.write(sentString);
     socket.end();
   }
 
   return { write: write, end: end };
 }
+
+// Date: new Date(),
+// Connection: "keep-alive",
+// "Keep-Alive": "timeout=5",
+// "Transfer-Encoding": "chunked",

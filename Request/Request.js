85225d1586e68fe3d4f3ea1f6abe962a9cf69d22
Yash Baddi <yashbaddi29@gmail.com>
2023-05-16 10:05:03 +0530

    Request.js modified

--- Request/Request.js
+++ Request/Request.js
@@ -1,7 +1,3 @@
-import { parseHeadersOnce } from "./req-utils.js";
-
-export default class Request {
-  constructor(input) {
-    Object.assign(this, parseHeadersOnce(input));
-  }
+class Request {
+  constructor(input) {}
 }
